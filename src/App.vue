aa8eca264bd5b979c4e521c8cf1e99939ce49305
ToyleBai <867447312@qq.com>
2021-07-22 22:04:54 +0800

    create resume

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+<style lang="css">
+html body {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+  min-height: 500px;
+  display: flex;
+  flex-direction: column;
+}
+
+ul p {
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  list-style: none;
+}
+</style>
---
+++ src/views/home/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">个人简历</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+};
+</script>
+<style lang="css" scoped>
+.header {
+  width: 100%;
+  height: 340px;
+  background: linear-gradient(to top, #373737, #212121);
+  line-height: 340px;
+  text-align: center;
+  font-size: 42px;
+  font-weight: 700;
+  text-shadow: #111 0px 0px 10px;
+  color: #fff;
+}
+</style>
