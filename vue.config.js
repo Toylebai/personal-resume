e1a67b3b2dbfeb1c99fd2770725b842720d24a78
ToyleBai <867447312@qq.com>
2021-07-22 22:50:20 +0800

    create resume

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/personal-resume" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/personal-resume/" : "/",
 };
